71436125666da0d801355981a2b474ac812d493d
Paolo <paolo.dincau@gmail.com>
2013-05-06 15:43:47 +0200

    first commit

---
+++ src/gcm_app.erl
@@ -0,0 +1,29 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo D'Incau <paolo.dincau@gmail.com>
+%%% @copyright (C) 2013, Paolo D'Incau
+%%% @doc
+%%%
+%%% @end
+%%% Created : 18 Apr 2013 by Paolo D'Incau <paolo.dincau@gmail.com>
+%%%-------------------------------------------------------------------
+-module(gcm_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/0]).
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start() ->
+    application:start(inets),
+    application:start(gcm).
+
+start(_StartType, _StartArgs) ->
+    gcm_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/gcm_sup.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo D'Incau <paolo.dincau@gmail.com>
+%%% @copyright (C) 2013, Paolo D'Incau
+%%% @doc
+%%%
+%%% @end
+%%% Created : 18 Apr 2013 by Paolo D'Incau <paolo.dincau@gmail.com>
+%%%-------------------------------------------------------------------
+-module(gcm_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, start_child/2]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, transient, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+start_child(Name, ApiKey) ->
+    supervisor:start_child(?MODULE, [Name, ApiKey]).
+
+init([]) ->
+    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(gcm, worker)]} }.
+
