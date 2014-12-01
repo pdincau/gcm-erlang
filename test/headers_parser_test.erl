954b13ffeb8bb98c8cb001c8c83b1cec46b3556e
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-01 21:24:55 +0100

    Fixing tests

--- test/headers_parser_test.erl
+++ test/headers_parser_test.erl
@@ -9,12 +9,11 @@ get_retry_after_when_set_test() ->
     TargetTime = qdate:to_string("Y-m-d h:ia",TargetDate),
     Headers = [{"retry-after", TargetTime}],
     ?assertMatch({ok,I} when is_integer(I), headers_parser:retry_after_from(Headers)).
- 
+
 get_retry_after_when_set_as_string_test() ->
     Headers = [{"retry-after", "120"}],
     ?assertEqual({ok,120}, headers_parser:retry_after_from(Headers)).
- 
+
 get_retry_after_when_not_set_test() ->
     Headers = [],
-    ?assertEqual(no_retry, headers_parser:retry_after_from(Headers)),.
-    
+    ?assertEqual(no_retry, headers_parser:retry_after_from(Headers)).
