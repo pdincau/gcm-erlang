5d6a6e25c904d45a5705b0daaeb49744dffaf72d
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-01 20:38:40 +0100

    Improving test

--- test/headers_parser_test.erl
+++ test/headers_parser_test.erl
@@ -3,21 +3,18 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-get_retry_after_test() ->
+get_retry_after_when_set_test() ->
     application:start(qdate),
     TargetDate = qdate:add_seconds(120, qdate:unixtime()),
     TargetTime = qdate:to_string("Y-m-d h:ia",TargetDate),
     Headers = [{"retry-after", TargetTime}],
-    ?assertMatch({ok,I} when is_integer(I), headers_parser:retry_after_from(Headers)),
-    pass.
+    ?assertMatch({ok,I} when is_integer(I), headers_parser:retry_after_from(Headers)).
  
-get_retry_after_interval_test() ->
+get_retry_after_when_set_as_string_test() ->
     Headers = [{"retry-after", "120"}],
-    ?assertEqual({ok,120}, headers_parser:retry_after_from(Headers)),
-    pass.
+    ?assertEqual({ok,120}, headers_parser:retry_after_from(Headers)).
  
-get_retry_after_not_set_test() ->
+get_retry_after_when_not_set_test() ->
     Headers = [],
-    ?assertEqual(no_retry, headers_parser:retry_after_from(Headers)),
-    pass.
+    ?assertEqual(no_retry, headers_parser:retry_after_from(Headers)),.
     
