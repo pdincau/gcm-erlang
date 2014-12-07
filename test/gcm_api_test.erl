a19e969fb3b4dd928111b818207deeba9d9e5d39
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-07 12:45:56 +0100

    Renaming tests

--- test/gcm_api_test.erl
+++ test/gcm_api_test.erl
@@ -3,17 +3,17 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-get_retry_after_when_set_test() ->
+get_retry_after_when_present_test() ->
     application:start(qdate),
     TargetDate = qdate:add_seconds(120, qdate:unixtime()),
     TargetTime = qdate:to_string("Y-m-d h:ia",TargetDate),
     Headers = [{"retry-after", TargetTime}],
     ?assertMatch({ok, I} when is_integer(I), gcm_api:retry_after_from(Headers)).
 
-get_retry_after_when_set_as_string_test() ->
+get_retry_after_when_present_as_string_test() ->
     Headers = [{"retry-after", "120"}],
     ?assertEqual({ok, 120}, gcm_api:retry_after_from(Headers)).
 
-get_retry_after_when_not_set_test() ->
+get_retry_after_when_missing_test() ->
     Headers = [],
     ?assertEqual(no_retry, gcm_api:retry_after_from(Headers)).
