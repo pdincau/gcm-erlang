cb1828d81d7ec489af54b67fc2395e2d485a328c
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-09 21:57:28 +0100

    Removing autogenerated comments

--- src/gcm_sup.erl
+++ src/gcm_sup.erl
@@ -1,34 +1,15 @@
-%%%-------------------------------------------------------------------
-%%% @author Paolo D'Incau <paolo.dincau@gmail.com>
-%%% @copyright (C) 2013, Paolo D'Incau
-%%% @doc
-%%%
-%%% @end
-%%% Created : 18 Apr 2013 by Paolo D'Incau <paolo.dincau@gmail.com>
-%%%-------------------------------------------------------------------
 -module(gcm_sup).
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0, start_child/3]).
 
-%% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, transient, 5000, Type, [I]}).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
 
 start_child(Name, ApiKey, ErrorFun) ->
     supervisor:start_child(?MODULE, [Name, ApiKey, ErrorFun]).
