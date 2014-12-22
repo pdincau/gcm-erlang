c8000596d86ef057161caf9f768fb632f73cb05d
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-12-22 16:59:35 +0100

    Adding spec to gcm_sup.erl

--- src/gcm_sup.erl
+++ src/gcm_sup.erl
@@ -7,12 +7,16 @@
 
 -define(CHILD(I, Type), {I, {I, start_link, []}, transient, 5000, Type, [I]}).
 
+-spec start_link() -> {ok, pid()}.
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+-spec start_child(atom(),string(),fun()) ->
+       {'error',_} | {'ok','undefined' | pid()} | {'ok','undefined' | pid(),_}.
 start_child(Name, ApiKey, ErrorFun) ->
     supervisor:start_child(?MODULE, [Name, ApiKey, ErrorFun]).
 
+-spec init([]) -> {ok, {{supervisor:strategy(), 5, 10}, [supervisor:child_spec()]}}.
 init([]) ->
-    {ok, { {simple_one_for_one, 5, 10}, [?CHILD(gcm, worker)]} }.
+    {ok, {{simple_one_for_one, 5, 10}, [?CHILD(gcm, worker)]}}.
 
