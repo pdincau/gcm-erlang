be45763f0ba5f8cf303e638f7bc31e9b77ff66d3
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-06-01 17:49:43 +0200

    Improving code indentation

--- src/gcm_api.erl
+++ src/gcm_api.erl
@@ -20,27 +20,27 @@ push(RegIds, Message, Key) ->
             error_logger:info_msg("Result was: ~p~n", [Json]),
             {ok, result_from(Json)};
         {ok, {{_, 400, _}, _, Body}} ->
-	    error_logger:error_msg("Error in request. Reason was: Bad Request - ~p~n", [Body]),
+            error_logger:error_msg("Error in request. Reason was: Bad Request - ~p~n", [Body]),
             {error, Body};
         {ok, {{_, 401, _}, _, _}} ->
-	    error_logger:error_msg("Error in request. Reason was: authorization error~n", []),
+            error_logger:error_msg("Error in request. Reason was: authorization error~n", []),
             {error, auth_error};
         {ok, {{_, Code, _}, Headers, _}} when Code >= 500 andalso Code =< 599 ->
-	    RetryTime = retry_after_from(Headers),
-	    error_logger:error_msg("Error in request. Reason was: retry. Will retry in: ~p~n", [RetryTime]),
+            RetryTime = retry_after_from(Headers),
+            error_logger:error_msg("Error in request. Reason was: retry. Will retry in: ~p~n", [RetryTime]),
             {error, {retry, RetryTime}};
         {ok, {{_StatusLine, _, _}, _, _Body}} ->
-	    error_logger:error_msg("Error in request. Reason was: timeout~n", []),
+            error_logger:error_msg("Error in request. Reason was: timeout~n", []),
             {error, timeout};
         {error, Reason} ->
-	    error_logger:error_msg("Error in request. Reason was: ~p~n", [Reason]),
+            error_logger:error_msg("Error in request. Reason was: ~p~n", [Reason]),
             {error, Reason};
         OtherError ->
-	    error_logger:error_msg("Error in request. Reason was: ~p~n", [OtherError]),
+            error_logger:error_msg("Error in request. Reason was: ~p~n", [OtherError]),
             {noreply, unknown}
     catch
         Exception ->
-	    error_logger:error_msg("Error in request. Exception ~p while calling URL: ~p~n", [Exception, ?BASEURL]),
+            error_logger:error_msg("Error in request. Exception ~p while calling URL: ~p~n", [Exception, ?BASEURL]),
             {error, Exception}
     end.
 
@@ -64,14 +64,14 @@ result_from(Json) ->
 -spec retry_after_from(headers()) -> 'no_retry' | non_neg_integer().
 retry_after_from(Headers) ->
     case proplists:get_value("retry-after", Headers) of
-	undefined ->
-	    no_retry;
-	RetryTime ->
-	    case string:to_integer(RetryTime) of
-		{Time, _} when is_integer(Time) ->
-		    Time;
-		{error, no_integer} ->
-		    Date = qdate:to_unixtime(RetryTime),
-		    Date - qdate:unixtime()
-	    end
+        undefined ->
+            no_retry;
+        RetryTime ->
+            case string:to_integer(RetryTime) of
+                {Time, _} when is_integer(Time) ->
+                    Time;
+                {error, no_integer} ->
+                    Date = qdate:to_unixtime(RetryTime),
+                    Date - qdate:unixtime()
+            end
     end.
