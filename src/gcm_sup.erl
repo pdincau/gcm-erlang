8072c6a77ac43a809aaeea4ca847cac3725e61d2
Karim Gemayel <ka.gemayel@gmail.com>
2016-02-24 13:57:35 +0100

    [gcm_sup] start hackney pool on gcm_sup child start

--- src/gcm_sup.erl
+++ src/gcm_sup.erl
@@ -14,9 +14,22 @@ start_link() ->
 -spec start_child(atom(),string()) ->
        {'error',_} | {'ok','undefined' | pid()} | {'ok','undefined' | pid(),_}.
 start_child(Name, ApiKey) ->
+    Pool = get_pool(),
+    PoolName = proplists:get_value(pool_name, Pool, mypool),
+    start_hackney(PoolName, hackney_options(Pool)),
     supervisor:start_child(?MODULE, [Name, ApiKey]).
 
 -spec init([]) -> {ok, {{supervisor:strategy(), 5, 10}, [supervisor:child_spec()]}}.
 init([]) ->
     {ok, {{simple_one_for_one, 5, 10}, [?CHILD(gcm, worker)]}}.
 
+
+get_pool() ->
+    {ok, Config} = application:get_env(gcm, hackney).
+
+start_hackney(PoolName, Options) ->
+    hackney_pool:start_pool(PoolName, Options).
+
+hackney_options(Pool) ->
+    [{timeout,         proplists:get_value(timeout, Pool, 150000)},
+     {max_connections, proplists:get_value(max_connections, Pool, 100)}].
